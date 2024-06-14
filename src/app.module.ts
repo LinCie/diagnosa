38a00d81ba3a344bf3892065195ea1b61e63a1cf
LinCie <aldiofernanda@protonmail.com>
2024-06-14 22:44:35 +0700

    added mikroorm imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SqliteDriver } from '@mikro-orm/sqlite';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'database.sqlite3',
+      driver: SqliteDriver,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
